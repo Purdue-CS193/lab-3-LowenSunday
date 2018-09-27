bfa75ea81a0dabb2f42637f72ae172117adf22cb
lu677 <luwh2000@gmail.com>
2018-09-27 09:22:29 -0400

    Change names

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,12 +3,11 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Wenhao\"" +
+                ", last = " + "\"Lu\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Wenhao", "Lu"));
     }
 }
-
 
 
